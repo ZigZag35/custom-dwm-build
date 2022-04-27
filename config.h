e3572b5b9a97cd3e219275c971bf282fbccd2d72
ZigZag35 <rayenpilote0@gmail.com>
2022-04-27 21:04:43 +0000

    update

--- config.h
+++ config.h
@@ -83,7 +83,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "run", NULL };
-static const char *bgcmd[] = { "~/.fehbg" };
+static const char *bgcmd[] = { "bash", "~/.fehbg", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
